e5b066cbcfa8b2707ac1c0efddf224f16d84b217
Jonah Kuria <jonah.kuria@hotmail.com>
2016-11-06 22:13:43 +0200

    create project and add login view controller

---
+++ SwiftyFriends/SwiftyFriends/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  SwiftyFriends
+//
+//  Created by Jonah Kuria on 2016/11/06.
+//  Copyright © 2016 Jonah Kuria. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+}
