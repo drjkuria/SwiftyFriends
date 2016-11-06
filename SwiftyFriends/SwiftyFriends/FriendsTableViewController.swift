afd049d73a1a1a24f7bfbaffaeb8c900687c9716
Jonah Kuria <jonah.kuria@hotmail.com>
2016-11-07 00:41:58 +0200

    implement gettting all friends in the friends table view controller and load images async

--- SwiftyFriends/SwiftyFriends/FriendsTableViewController.swift
+++ SwiftyFriends/SwiftyFriends/FriendsTableViewController.swift
@@ -7,90 +7,83 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class FriendsTableViewController: UITableViewController {
     
     var uniqueID: String?
     var firstName: String?
     var jsonArray:NSMutableArray?
+    var friends: Array<Friend> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Friends"
         self.navigationItem.hidesBackButton = true
+        
+        let friendsEndpoint: String = "http://mobileexam.dstv.com/friends.php" +
+        "?name=" + firstName! + ";uniqueID=" + uniqueID!
+        
+        
+        Alamofire.request(friendsEndpoint, method: .post)
+        .responseJSON { response in
+            
+            if let getResponse = response.result.value {
+                let jsonResult = JSON(getResponse)
+                var friendDict = [String: String]()
+                if let items = jsonResult["friends"].array {
+                    for item in items {
+                        for (key, value) in item {
+                            friendDict[key] = value.rawString()!
+                        }
+                        let friend = Friend(data: friendDict)
+                        self.friends.append(friend)
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.tableView.reloadData()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.friends.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath)
+        
+        //        load image async
+        let urlString = self.friends[indexPath.row].imageURL
+        URLSession.shared.dataTask(with: NSURL(string: urlString!)! as URL, completionHandler: { (data, response, error) -> Void in
+            
+            if error != nil {
+                print(error)
+                return
+            }
+            DispatchQueue.main.async(execute: { () -> Void in
+                let image = UIImage(data: data!)
+                if let cellToUpdate = tableView.cellForRow(at: indexPath) {
+                    cellToUpdate.imageView?.image = image
+                }
+            })
+            
+        }).resume()
+        
+        cell.textLabel?.text = self.friends[indexPath.row].alias
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
